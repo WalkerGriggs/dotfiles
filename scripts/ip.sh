43ff8fbd481eb1b697638287a47521b83d5eec6c
Walker <wgriggs@DebBox.walkergriggs.com>
2017-10-01 16:29:20 -0400

    updated ip script

--- scripts/ip.sh
+++ scripts/ip.sh
@@ -2,6 +2,8 @@
 
 if [ $(pgrep -x openvpn) ]; then
   dig TXT +short o-o.myaddr.l.google.com @ns1.google.com | awk -F'"' '{ print $2}'
+elif [ $(nmcli --terse -f DEVICE,TYPE conn show --active | grep ethernet) ]; then
+  ip addr show enp4s0 | grep -w inet | awk '{print $2}' | sed 's/\/.*//'
 else
   ip addr show wlp3s0 | grep -w inet | awk '{print $2}' | sed 's/\/.*//'
 fi
