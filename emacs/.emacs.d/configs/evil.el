c3904507f5f6d36f30053e54261289cf7b098cc6
WalkerGriggs <walker@walkergriggs.com>
2020-02-09 00:29:13 -0800

    Updates evil

--- emacs/.emacs.d/configs/evil.el
+++ emacs/.emacs.d/configs/evil.el
@@ -15,27 +15,22 @@
   :config
   (evil-mode 1)
 
-  (define-key evil-normal-state-map (kbd "C-h") 'evil-window-left)
-  (define-key evil-normal-state-map (kbd "C-j") 'evil-window-down)
-  (define-key evil-normal-state-map (kbd "C-k") 'evil-window-up)
-  (define-key evil-normal-state-map (kbd "C-l") 'evil-window-right)
-
-  (define-key evil-normal-state-map (kbd "SPC SPC") 'helm-M-x)
-
   (require 'evil-leader)
   (require 'evil-surround)
 
-  (setq evil-default-cursor t)
+  (setq evil-default-cursor t
+        evil-normal-state-cursor 'box
+        evil-visual-state-cursor 'hollow)
 
   (with-eval-after-load 'evil-maps
-    (define-key evil-motion-state-map (kbd "/") 'swiper))
+    (define-key evil-motion-state-map (kbd "/") 'swiper)
+    (define-key evil-normal-state-map (kbd "SPC SPC") 'counsel-M-x))
 
   (evil-leader/set-leader ",")
   (evil-leader/set-key
-    "g" 'magit-status
+    "s" 'magit-status
     "k" 'kill-buffer
     "K" 'kill-this-buffer
-    "l" 'kill-whole-line
     "t" 'ansi-term
     "w" 'save-buffer
     ))
